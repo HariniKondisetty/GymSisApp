4fe43c57f9ee44bdf23c54be10e46d8ee26803d7
Harini Kondisetty <harini.kondisetty@gmail.com>
2025-07-14 13:03:55 -0500

    Initial Commit

---
+++ GymSisApp/GymSisAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymSisAppApp.swift
+//  GymSisApp
+//
+//  Created by Scholar on 7/14/25.
+//
+
+import SwiftUI
+
+@main
+struct GymSisAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
