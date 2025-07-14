dbae5e156fae9d44ef9e04827e17cddff30e9d6e
Harini Kondisetty <harini.kondisetty@gmail.com>
2025-07-14 13:11:35 -0500

    Made a text change

--- GymSisApp/ContentView.swift
+++ GymSisApp/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
-            Text("GymSis")
+            Text("GymSis3")
         }
         .padding()
     }
